3f5b8869827ebe96fe3d3a81c17adc3eb379f80b
Luis Cusihuaman <luiscusihuaman88@gmail.com>
2021-08-27 22:06:49 -0300

    chore: fix deprecated warnings

--- README.md
+++ README.md
@@ -8,6 +8,15 @@
 
 `make start-local`
 
+`docker-compose up -d --build`
+
 `php apps/mooc/backend/bin/console luiscusihuaman:consume-domain-events:mysql <quantityEventsToProcess>`
 
 `curl http://localhost:8030/health-check`
+
+## Tests
+
+`docker-compose exec php vendor/bin/phpunit`
+
+`docker-compose exec php vendor/bin/behat -p mooc_backend`
+
--- apps/bootstrap.php
+++ apps/bootstrap.php
@@ -9,9 +9,11 @@ require $rootPath . '/vendor/autoload.php';
 
 // Load cached env vars if the .env.local.php file exists
 // Run "composer dump-env prod" to create it (requires symfony/flex >=1.2)
-if (is_array($env = @include $rootPath . '/.env.local.php')) {
-    foreach ($env as $k => $v) {
-        $_ENV[$k] = $_ENV[$k] ?? (isset($_SERVER[$k]) && 0 !== strpos($k, 'HTTP_') ? $_SERVER[$k] : $v);
+if (file_exists($rootPath . '/.env.local.php')) {
+    if (is_array($env = @include $rootPath . '/.env.local.php')) {
+        foreach ($env as $k => $v) {
+            $_ENV[$k] = $_ENV[$k] ?? (isset($_SERVER[$k]) && 0 !== strpos($k, 'HTTP_') ? $_SERVER[$k] : $v);
+        }
     }
 } elseif (!class_exists(Dotenv::class)) {
     throw new RuntimeException('Please run "composer require symfony/dotenv" to load the ".env" files configuring the application.');

1beadd3f6413460ab8e43f58d34f4fd808521203
Luis Cusihuaman <luiscusihuaman88@gmail.com>
2021-11-27 19:39:32 -0300

    feat: backoffice home with twig

--- apps/backoffice/frontend/src/Controller/Home/HomeGetController.php
+++ apps/backoffice/frontend/src/Controller/Home/HomeGetController.php
@@ -4,15 +4,24 @@ declare(strict_types=1);
 
 namespace LuisCusihuaman\Apps\Backoffice\Frontend\Controller\Home;
 
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Twig\Environment;
 
 final class HomeGetController
 {
+    public function __construct(Environment $twig)
+    {
+        $this->twig = $twig;
+    }
 
     public function __invoke(Request $request): Response
     {
-        return new JsonResponse(['backoffice-home!' => 'ok']);
+        return new Response($this->twig->render(
+            'pages/home.html.twig',
+            [
+                'title' => 'Welcome',
+                'description' => 'LuisCusihuaman - Backoffice',
+            ]));
     }
 }

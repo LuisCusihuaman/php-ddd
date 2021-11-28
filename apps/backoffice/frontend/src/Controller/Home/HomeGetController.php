27bd33c8ce986134bfb7e94ef74378767085d1c3
Luis Cusihuaman <luiscusihuaman88@gmail.com>
2021-11-28 08:43:07 -0300

    refactor: abstract twig controller

--- apps/backoffice/frontend/src/Controller/Home/HomeGetController.php
+++ apps/backoffice/frontend/src/Controller/Home/HomeGetController.php
@@ -4,24 +4,19 @@ declare(strict_types=1);
 
 namespace LuisCusihuaman\Apps\Backoffice\Frontend\Controller\Home;
 
+use LuisCusihuaman\Shared\Infrastructure\Symfony\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Twig\Environment;
 
-final class HomeGetController
+final class HomeGetController extends Controller
 {
-    public function __construct(Environment $twig)
-    {
-        $this->twig = $twig;
-    }
-
     public function __invoke(Request $request): Response
     {
-        return new Response($this->twig->render(
+        return $this->render(
             'pages/home.html.twig',
             [
                 'title' => 'Welcome',
                 'description' => 'LuisCusihuaman - Backoffice',
-            ]));
+            ]);
     }
 }
---
+++ src/Shared/Infrastructure/Symfony/Controller.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace LuisCusihuaman\Shared\Infrastructure\Symfony;
+
+use Symfony\Component\HttpFoundation\Response;
+use Twig\Environment;
+
+abstract class Controller
+{
+    private $twig;
+
+    public function __construct(Environment $twig)
+    {
+        $this->twig = $twig;
+    }
+
+    public function render(string $templatePath, array $arguments = []): Response
+    {
+        return new Response($this->twig->render($templatePath, $arguments));
+    }
+}

5df8966c35d8af02e107f7e1690cd1c4434c3b0a
Luis Cusihuaman <luiscusihuaman88@gmail.com>
2021-11-28 11:28:17 -0300

    feat(backoffice/course-counter): page ask a query to the bus for data

--- apps/backoffice/frontend/src/Controller/Courses/CoursesGetController.php
+++ apps/backoffice/frontend/src/Controller/Courses/CoursesGetController.php
@@ -2,20 +2,34 @@
 
 namespace LuisCusihuaman\Apps\Backoffice\Frontend\Controller\Courses;
 
+use LuisCusihuaman\Mooc\CoursesCounter\Application\Find\CoursesCounterResponse;
+use LuisCusihuaman\Mooc\CoursesCounter\Application\Find\FindCoursesCounterQuery;
+use LuisCusihuaman\Mooc\CoursesCounter\Domain\CoursesCounterNotExist;
 use LuisCusihuaman\Shared\Infrastructure\Symfony\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Messenger\Exception\HandlerFailedException;
 
 final class CoursesGetController extends Controller
 {
     public function __invoke(Request $request): Response
     {
+        try {
+            /** @var CoursesCounterResponse $response */
+            $response = $this->ask(new FindCoursesCounterQuery());
+            $totalCourses = $response->total();
+        } catch (HandlerFailedException $ex) {
+            if ($ex->getPrevious() instanceof CoursesCounterNotExist) {
+                $totalCourses = 0;
+            }
+        }
+
         return $this->render(
             'pages/courses.html.twig',
             [
                 'title' => 'Courses',
                 'description' => 'Backoffice',
-                'courses_counter' => 10,
+                'courses_counter' => $totalCourses,
             ]
         );
     }
--- src/Shared/Infrastructure/Symfony/Controller.php
+++ src/Shared/Infrastructure/Symfony/Controller.php
@@ -2,20 +2,30 @@
 
 namespace LuisCusihuaman\Shared\Infrastructure\Symfony;
 
-use Symfony\Component\HttpFoundation\Response;
+use LuisCusihuaman\Shared\Domain\Bus\Query\Query;
+use LuisCusihuaman\Shared\Domain\Bus\Query\QueryBus;
+use LuisCusihuaman\Shared\Domain\Bus\Query\Response;
+use Symfony\Component\HttpFoundation\Response as SymfonyResponse;
 use Twig\Environment;
 
 abstract class Controller
 {
     private $twig;
+    private $queryBus;
 
-    public function __construct(Environment $twig)
+    public function __construct(Environment $twig, QueryBus $queryBus)
     {
         $this->twig = $twig;
+        $this->queryBus = $queryBus;
     }
 
-    public function render(string $templatePath, array $arguments = []): Response
+    public function render(string $templatePath, array $arguments = []): SymfonyResponse
     {
-        return new Response($this->twig->render($templatePath, $arguments));
+        return new SymfonyResponse($this->twig->render($templatePath, $arguments));
+    }
+
+    public function ask(Query $query): ?Response
+    {
+        return $this->queryBus->ask($query);
     }
 }

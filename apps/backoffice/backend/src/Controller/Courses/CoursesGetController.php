95bc539644f1a5ae2eb26db23d198d5f9b18f916
Luis Cusihuaman <luiscusihuaman88@gmail.com>
2021-12-06 23:19:23 -0300

    feat: split backoffice frontend and backend

---
+++ apps/backoffice/backend/src/Controller/Courses/CoursesGetController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace LuisCusihuaman\Apps\Backoffice\Backend\Controller\Courses;
+
+use LuisCusihuaman\Backoffice\Courses\Application\BackofficeCourseResponse;
+use LuisCusihuaman\Backoffice\Courses\Application\BackofficeCoursesResponse;
+use LuisCusihuaman\Backoffice\Courses\Application\SearchByCriteria\SearchBackofficeCoursesByCriteriaQuery;
+use LuisCusihuaman\Shared\Domain\Bus\Query\QueryBus;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Messenger\Exception\HandlerFailedException;
+use function Lambdish\Phunctional\map;
+
+final class CoursesGetController
+{
+    private $queryBus;
+
+    public function __construct(QueryBus $queryBus)
+    {
+        $this->queryBus = $queryBus;
+    }
+
+    public function __invoke(Request $request): JsonResponse
+    {
+        $totalCourses = 0;
+
+        try {
+            /** @var BackofficeCoursesResponse $response */
+            $response = $this->queryBus->ask(
+                new SearchBackofficeCoursesByCriteriaQuery(
+                    $request->query->get('filters', []),
+                    $request->query->get('order_by'),
+                    $request->query->get('order'),
+                    $request->query->get('limit'),
+                    $request->query->get('offset')
+                )
+            );
+        } catch (HandlerFailedException $ex) {
+        }
+
+        return new JsonResponse(
+            map($this->toArray(), $response->courses()),
+            200,
+            ['Access-Control-Allow-Origin' => '*']
+        );
+    }
+
+    private function toArray(): callable
+    {
+        return static function (BackofficeCourseResponse $course) {
+            return [
+                'id' => $course->id(),
+                'name' => $course->name(),
+                'duration' => $course->duration(),
+            ];
+        };
+    }
+}
